5a1f76a8a45a484e3f51535413c29787a45cf5bb
Lucas Petit <surajpetit@gmail.com>
2026-01-19 00:02:05 +0100

    PAP : reorganise structure for listing 39

--- Performance Aware Programming/main.cpp
+++ Performance Aware Programming/main.cpp
@@ -13,7 +13,7 @@ enum class ErrorCode
     FileNotFound,
 };
 
-void print_error(const ErrorCode& error) 
+void print_error(const ErrorCode& error) noexcept
 {
     switch (error) 
     {
@@ -26,7 +26,7 @@ void print_error(const ErrorCode& error)
     }
 }
 
-std::expected<std::vector<std::byte>, ErrorCode> load_binary_file(const std::string& path) 
+[[nodiscard]] std::expected<std::vector<std::byte>, ErrorCode> load_binary_file(const std::string& path) 
 {
     std::ifstream file(path, std::ios::binary | std::ios::ate);
     if (!file) 
@@ -43,53 +43,88 @@ std::expected<std::vector<std::byte>, ErrorCode> load_binary_file(const std::str
     return buffer;
 }
 
+int register_or_memory_to_register(std::span<const std::byte, 4> binary_data)
+{
+    return 0;
+}
+
 void print_mnemonics(std::span<const std::byte> binary_data) noexcept
 {
     std::println("bits 16");
-    for (const auto& chunk : binary_data | std::views::chunk(2)) 
+    
+    int step = 1;
+    for (size_t i = 0; i < binary_data.size(); i += step) 
     {
-        if (chunk.size() < 2) 
-        {
-            continue; // Skip incomplete instruction
-        }
 
-        std::byte byte_one = chunk[0];
-        std::byte byte_two = chunk[1];
+        std::byte byte_one = binary_data[i];
+        std::byte byte_two = binary_data[i + 1];
 
         int opcode = (std::to_integer<int>(byte_one) >> 2) & 0x3F;
 
-        if (opcode != 0b100010) 
+        if (opcode == 0b100010) 
+        {
+            bool direction  =  std::to_integer<int>(byte_one) & 0x2;
+            bool w          =  std::to_integer<int>(byte_one) & 0x1;
+            int mod         = (std::to_integer<int>(byte_two) >> 6) & 0x3;
+            int reg         = (std::to_integer<int>(byte_two) >> 3) & 0x7;
+            int rm          =  std::to_integer<int>(byte_two) & 0x7;
+
+            if (mod == 0b00 && rm != 0b110)
+            {
+                static constexpr std::string_view reg_names_8bit[]  = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
+                static constexpr std::string_view reg_names_16bit[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
+
+                std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
+                std::string_view rm_name  = w ? reg_names_16bit[rm]  : reg_names_8bit[rm];
+
+                if (direction) 
+                {
+                    std::println("mov {}, {}", reg_name, rm_name);
+                } 
+                else 
+                {
+                    std::println("mov {}, {}", rm_name, reg_name);
+                }
+                step = 2;
+            }
+            else if (mod == 0b01)
+            {
+                step = 3;
+            }
+            else if (mod == 0b10 || mod == 0b00 && rm == 0b110)
+            {
+                step = 4;
+            }
+            else if (mod == 0b11)
+            {
+                step = 2;
+            }
+        }
+        else if (opcode >> 2 & 0b1011)
+        {
+            bool w          = (std::to_integer<int>(byte_one) >> 3) & 0x1;
+            int reg         = std::to_integer<int>(byte_one) & 0x7;
+            if (w)
+            {
+                step = 3;
+            }
+            else
+            {
+                step = 2;
+            }
+        }
+        else 
         {
             return; // Parse only MOV instructions
         }
 
-        bool direction  =  std::to_integer<int>(byte_one) & 0x2;
-        bool w          =  std::to_integer<int>(byte_one) & 0x1;
-        int mod         = (std::to_integer<int>(byte_two) >> 6) & 0x3;
-        int reg         = (std::to_integer<int>(byte_two) >> 3) & 0x7;
-        int rm          =  std::to_integer<int>(byte_two) & 0x7;
-
-        static constexpr std::string_view reg_names_8bit[]  = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
-        static constexpr std::string_view reg_names_16bit[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
-
-        std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
-        std::string_view rm_name  = w ? reg_names_16bit[rm] : reg_names_8bit[rm];
-
-        if (direction) 
-        {
-            std::println("mov {}, {}", reg_name, rm_name);
-        } 
-        else 
-        {
-            std::println("mov {}, {}", rm_name, reg_name);
-        }
     }
 }
 
 void print_binary(std::span<const std::byte> binary_data)
 {
     std::print(";Binary representation of the file: \n;");
-    for (const auto row : binary_data | std::views::chunk(8)) 
+    for (const auto& row : binary_data | std::views::chunk(8)) 
     {
         for (const std::byte byte : row)
         {
@@ -109,7 +144,7 @@ void print_hex(std::span<const std::byte> data)
 
     std::size_t offset = 0;
 
-    for (auto row : data | std::views::chunk(16)) 
+    for (const auto& row : data | std::views::chunk(16)) 
     {
         std::print(";{:06x}: ", offset);
 
@@ -156,12 +191,14 @@ int main(int argc, char** argv)
         return 1;
     }
     
-    auto& binary_data = loaded_binary.value();
+    std::vector<std::byte>& binary_data = loaded_binary.value();
 
     std::println("; {} disassembly : \n", argv[1]);
     print_binary(binary_data);
+
     std::println("\n");
     print_hex(binary_data);
+    
     std::println();
     print_mnemonics(binary_data);
 
