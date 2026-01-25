2b99ed5b663d565ba056f2f2175c9007a4e23744
Lucas Petit <surajpetit@gmail.com>
2026-01-25 02:22:28 +0100

    PAP : refactor mov parsing into smaller functions for clarity \ Added target verify in the makefile to easily verify the result

--- Performance Aware Programming/main.cpp
+++ Performance Aware Programming/main.cpp
@@ -1,22 +1,25 @@
-#include <vector>
 #include <cstddef>
 #include <fstream>
 #include <string_view>
 #include <ranges>
 #include <print>
 #include <expected>
-#include <span>
 #include <cctype> // for std::isprint
+#include <array>
+#include <utility>
+#include <vector>
+#include <span>
 
 enum class ErrorCode 
 {
     FileNotFound,
+    FileReadError,
 };
 
-enum class InstructionEncoding
+enum class Instruction
 {
-    Register_Or_Memory_From_Or_To_Register = 0b100010,
-    Immediate_To_Register_Or_Memory
+    MOV_Register_Or_Memory_To_Register = 0b100010,
+    MOV_Immediate_To_Register_Or_Memory = 0b1011,
 };
 
 enum class ModeFieldEncoding
@@ -49,112 +52,136 @@ void print_error(const ErrorCode& error) noexcept
     }
 
     std::streamsize size = file.tellg();
-    file.seekg(0);
+    if (size < 0)
+    {
+        return std::unexpected(ErrorCode::FileNotFound);
+    }
 
-    std::vector<std::byte> buffer(size);
-    file.read(reinterpret_cast<char*>(buffer.data()), size);
+    file.seekg(0, std::ios::beg);
+
+    std::vector<std::byte> buffer(static_cast<std::size_t>(size));
+    if (!file.read(reinterpret_cast<char*>(buffer.data()), size))
+    {
+        return std::unexpected(ErrorCode::FileReadError);
+    }
 
     return buffer;
 }
 
-void print_mnemonics(std::span<const std::byte> binary_data) noexcept
-{
-    static constexpr std::string_view reg_names_8bit[]  = {"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"};
-    static constexpr std::string_view reg_names_16bit[] = {"ax", "cx", "dx", "bx", "sp", "bp", "si", "di"};
-    static constexpr std::string_view reg_adress_calculation[]  = { "bx + si", "bx + di", "bp + si", "bp + di",  "si",  "di",  "bp",  "bx" };
+static constexpr std::array<std::string_view, 8> reg_names_8bit = { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" };
+static constexpr std::array<std::string_view, 8> reg_names_16bit = { "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" };
+static constexpr std::array<std::string_view, 8> reg_adress_calculation = { "bx + si", "bx + di", "bp + si", "bp + di",  "si",  "di",  "bp",  "bx" };
 
+std::pair<std::string, size_t> decode_mode_field(std::span<const std::byte> binary_data, size_t offset, bool w, int mod, int rm)
+{
+    if (mod == std::to_underlying(ModeFieldEncoding::Register_Mode))
+    {
+        std::string rm_name(w ? reg_names_16bit[rm] : reg_names_8bit[rm]);
+        return {rm_name, 2 };
+    }
+
+    if (mod == std::to_underlying(ModeFieldEncoding::Memory_Mode_No_Displacement) && rm != 0b110)
+    {
+        return { std::format("[{}]", reg_adress_calculation[rm]), 2 };
+    }
+
+    std::byte byte_three = binary_data[offset + 2];
+    if (mod == std::to_underlying(ModeFieldEncoding::Memory_Mode_8_Bit_Displacement))
+    {
+        return { std::format("[{} + {}]", reg_adress_calculation[rm], std::to_integer<int>(byte_three)), 3};
+    }
+
+    std::byte byte_four = binary_data[offset + 3];
+    int displacement = std::to_integer<int>(byte_three) | (std::to_integer<int>(byte_four) << 8);
+
+    if (rm == 0b110 && mod == std::to_underlying(ModeFieldEncoding::Memory_Mode_No_Displacement))
+    {
+        return { std::format("[{}]", displacement), 4 };
+    }
+
+    if (mod == std::to_underlying(ModeFieldEncoding::Memory_Mode_16_Bit_Displacement))
+    {
+        return { std::format("[{} + {}]", reg_adress_calculation[rm], displacement), 4 };
+    }
+
+    return {"", 2};
+}
+
+size_t decode_register_memory_instruction(std::span<const std::byte> binary_data, size_t offset)
+{
+    std::byte byte_one = binary_data[offset];
+    std::byte byte_two = binary_data[offset + 1];
+
+    bool direction  =  std::to_integer<int>(byte_one) & 0x2;
+    bool w          =  std::to_integer<int>(byte_one) & 0x1;
+
+    int mod         = (std::to_integer<int>(byte_two) >> 6) & 0x3;
+    int reg         = (std::to_integer<int>(byte_two) >> 3) & 0x7;
+    int rm          =  std::to_integer<int>(byte_two) & 0x7;
+
+    std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
+
+    const auto& [rm_name, nb_bytes_read] = decode_mode_field(binary_data, offset, w, mod, rm);
+
+    if (direction)
+    {
+        std::println("mov {}, {}", reg_name, rm_name);
+    }
+    else
+    {
+        std::println("mov {}, {}", rm_name, reg_name);
+    }
+
+	return nb_bytes_read;
+}
+
+size_t decode_immediate_to_register_instruction(std::span<const std::byte> data, size_t offset)
+{
+	size_t nb_bytes_read = 0;
+    std::byte byte_one = data[offset];
+    std::byte byte_two = data[offset + 1];
+
+    bool w = (std::to_integer<int>(byte_one) >> 3) & 0x1;
+    int reg = std::to_integer<int>(byte_one) & 0x7;
+
+    std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
+
+    int immediate;
+    if (w)
+    {
+        std::byte byte_three = data[offset + 2];
+        immediate = std::to_integer<int>(byte_two) | (std::to_integer<int>(byte_three) << 8);
+        nb_bytes_read = 3;
+    }
+    else
+    {
+        immediate = std::to_integer<int>(byte_two);
+        nb_bytes_read = 2;
+    }
+
+    std::println("mov {}, {}", reg_name, immediate);
+    
+    return nb_bytes_read;
+}
+
+void print_mnemonics(std::span<const std::byte> binary_data) noexcept
+{   
     std::println("bits 16");
     
     int nb_bytes_read = 1;
-    for (size_t i = 0; i < binary_data.size(); i += nb_bytes_read) 
+    for (size_t i = 0; i < binary_data.size();) 
     {
         std::byte byte_one = binary_data[i];
-        std::byte byte_two = binary_data[i + 1];
 
         int opcode = (std::to_integer<std::uint8_t>(byte_one) >> 2) & 0x3F;
 
-        if (opcode == static_cast<int>(InstructionEncoding::Register_Or_Memory_From_Or_To_Register)) 
+        if (opcode == std::to_underlying(Instruction::MOV_Register_Or_Memory_To_Register)) 
         {
-            bool direction  =  std::to_integer<int>(byte_one) & 0x2;
-            bool w          =  std::to_integer<int>(byte_one) & 0x1;
-            int mod         = (std::to_integer<int>(byte_two) >> 6) & 0x3;
-            int reg         = (std::to_integer<int>(byte_two) >> 3) & 0x7;
-            int rm          =  std::to_integer<int>(byte_two) & 0x7;
-
-            std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
-            std::string rm_name;
-
-            if (mod == static_cast<int>(ModeFieldEncoding::Register_Mode))
-            {
-                rm_name = w ? reg_names_16bit[rm]  : reg_names_8bit[rm];
-                nb_bytes_read = 2;
-            }
-            else
-            {
-                if (mod == static_cast<int>(ModeFieldEncoding::Memory_Mode_No_Displacement) && rm != 0b110)
-                {
-                    rm_name = std::format("[{}]", reg_adress_calculation[rm]);
-                    nb_bytes_read = 2;
-                    
-                }
-                else if (mod == static_cast<int>(ModeFieldEncoding::Memory_Mode_8_Bit_Displacement))
-                {
-                    std::byte byte_three = binary_data[i + 2];
-                    rm_name = std::format("[{} + {}]", reg_adress_calculation[rm], std::to_integer<int>(byte_three));
-                    nb_bytes_read = 3;
-                }
-                else if 
-                (
-                    mod == static_cast<int>(ModeFieldEncoding::Memory_Mode_16_Bit_Displacement) 
-                    || mod == static_cast<int>(ModeFieldEncoding::Memory_Mode_No_Displacement) && rm == 0b110
-                )
-                {
-                    std::byte byte_three = binary_data[i + 2];
-                    std::byte byte_four  = binary_data[i + 3];
-                    int displacement = std::to_integer<int>(byte_three) | (std::to_integer<int>(byte_four) << 8);
-
-                    if (rm == 0b110 && mod == static_cast<int>(ModeFieldEncoding::Memory_Mode_No_Displacement))
-                    {
-                        rm_name = std::format("[{}]", displacement);
-                    }
-                    else
-                    {
-                        rm_name = std::format("[{} + {}]", reg_adress_calculation[rm], displacement);
-                    }
-                    nb_bytes_read = 4;
-                }
-            }
-            if (direction) 
-            {
-                std::println("mov {}, {}", reg_name, rm_name);
-            } 
-            else 
-            {
-                std::println("mov {}, {}", rm_name, reg_name);
-            }
+            i += decode_register_memory_instruction(binary_data, i);
         }
-        else if (opcode >> 2 == 0b1011)
+        else if (opcode >> 2 == std::to_underlying(Instruction::MOV_Immediate_To_Register_Or_Memory))
         {
-            bool w          = (std::to_integer<int>(byte_one) >> 3) & 0x1;
-            int reg         = std::to_integer<int>(byte_one) & 0x7;
-
-            std::string_view reg_name = w ? reg_names_16bit[reg] : reg_names_8bit[reg];
-
-            int immediate;
-            
-            if (w)
-            {
-                std::byte byte_three = binary_data[i + 2];
-                immediate = std::to_integer<int>(byte_two) | (std::to_integer<int>(byte_three) << 8);
-                nb_bytes_read = 3;
-            }
-            else
-            {
-                immediate = std::to_integer<int>(byte_two);
-                nb_bytes_read = 2;
-            }
-
-            std::println("mov {}, {}", reg_name, immediate);
+            i += decode_immediate_to_register_instruction(binary_data, i);
         }
         else 
         {
@@ -230,6 +257,7 @@ int main(int argc, char** argv)
     if (!loaded_binary) 
     {
         print_error(loaded_binary.error());
+        std::println("Given path : {}", argv[1]);
         return 1;
     }
     
